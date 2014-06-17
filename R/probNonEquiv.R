f1caeb283459b31f822d77e61faea83b4c6625aa
David Rossell <rosselldavid@gmail.com>
2014-06-17 12:02:42 +0000

    Fixed bug in probNonEquiv. Added pvalTreat
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/casper@91518 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/probNonEquiv.R
+++ R/probNonEquiv.R
@@ -4,9 +4,23 @@
 # - logfc is a biologically relevant threshold for the log-FC
 # - minCount: probabilities are only computed for genes with fData(x)$readCount >= minCount
 # - method: 'exact' for exact posterior probabilities (slower), 'plugin' for plug-in approximation (much faster)
-probNonEquiv <- function(x, groups, logfc=log(2), minCount, method='exact') {
-  #require(plyr)
-  if (class(x) != 'ExpressionSet') stop('x must be of class ExpressionSet')
+
+setMethod("probNonEquiv", signature(x='list'), function(x, groups, logfc=log(2), minCount, method="plugin", mc.cores=1) {
+  if (any(sapply(x,class) != 'ExpressionSet')) stop('All elements in x must be of class ExpressionSet')
+  if (mc.cores>1) {
+    if ('parallel' %in% loadedNamespaces()) {
+      ans <- parallel::mclapply(x, probNonEquiv, groups=groups, logfc=logfc, minCount=minCount, method=method, mc.cores=mc.cores, mc.preschedule=TRUE)
+    } else {
+      stop("parallel package has not been loaded!")
+    }
+  } else {
+    ans <- lapply(x, probNonEquiv, groups=groups, logfc=logfc, minCount=minCount, method=method)
+  }
+  do.call(cbind, ans)
+}
+)
+
+setMethod(probNonEquiv, signature(x="ExpressionSet"), function(x, groups, logfc=log(2), minCount, method="plugin", mc.cores=1) {
   if (missing(groups)) stop('groups must be specified')
   if (is.null(pData(x)[,groups])) stop(paste('Variable',groups,'not found in pData(x)'))
   if (!missing(minCount)) sel <- fData(x)$readCount >= minCount else sel <- rep(TRUE,nrow(x))
@@ -47,12 +61,12 @@ probNonEquiv <- function(x, groups, logfc=log(2), minCount, method='exact') {
     phimode <- ifelse(a.sigma>1, b.sigma/(a.sigma-1), b.sigma/a.sigma)
     v <- cbind(1/(ncolsel[1]/phimode + 1/par['tau02']), 1/(ncolsel[2]/phimode + 1/par['tau02']))
     m <- (t(ncolsel*t(xbar))/phimode + par['mu0']/par['tau02']) * v
-    #ans[sel] <- pnorm(-logfc,m[,1]-m[,2],sd=sqrt(v[1]+v[2])) + pnorm(logfc,m[,1]-m[,2],sd=sqrt(v[1]+v[2]),lower.tail=FALSE)
-    ans[sel] <- pt((-logfc-(m[,1]-m[,2]))/sqrt(v[1]+v[2]), df=ncolsel[1]) + pt((logfc-(m[,1]-m[,2]))/sqrt(v[1]+v[2]), df=ncolsel[2], lower.tail=FALSE)    
+    ans[sel] <- pnorm(-logfc,m[,1]-m[,2],sd=sqrt(v[,1]+v[,2])) + pnorm(logfc,m[,1]-m[,2],sd=sqrt(v[,1]+v[,2]),lower.tail=FALSE)
+    #ans[sel] <- pt((-logfc-(m[,1]-m[,2]))/sqrt(v[,1]+v[,2]), df=ncolsel[1]) + pt((logfc-(m[,1]-m[,2]))/sqrt(v[,1]+v[,2]), df=ncolsel[2], lower.tail=FALSE)
   }
   return(ans)
 } 
-
+)
 
 dinvgammaVec <- function(x, alpha, beta, logscale=TRUE) {
     ans <- alpha * log(beta) - lgamma(alpha) - (alpha + 1) * log(x) - (beta/x)
@@ -61,45 +75,29 @@ dinvgammaVec <- function(x, alpha, beta, logscale=TRUE) {
 }
 
 
+setMethod("pvalTreat", signature(x='list'), function(x, groups, logfc=log(2), minCount, p.adjust.method='none', mc.cores=1) {
+  if (any(sapply(x,class) != 'ExpressionSet')) stop('All elements in x must be of class ExpressionSet')
+  if (mc.cores>1) {
+    if ('parallel' %in% loadedNamespaces()) {
+      ans <- parallel::mclapply(x, pvalTreat, groups=groups, logfc=logfc, minCount=minCount, p.adjust.method=p.adjust.method, mc.cores=mc.cores, mc.preschedule=TRUE)
+    } else {
+      stop("parallel package has not been loaded!")
+    }
+  } else {
+    ans <- lapply(x, pvalTreat, groups=groups, logfc=logfc, minCount=minCount, p.adjust.method=p.adjust.method)
+  }
+  do.call(cbind, ans)
+}
+)
 
-probNonEquiv2 <- function(x, groups, logfc=log(2), minCount) {
-  #same as postProb, uses quantile integration
-  #require(gaga)
-  #require(plyr)
-  if (missing(groups)) stop('groups must be specified')
+setMethod("pvalTreat", signature(x='ExpressionSet'), function(x, groups, logfc=log(2), minCount, p.adjust.method='none', mc.cores=1) {
   if (is.null(pData(x)[,groups])) stop(paste('Variable',groups,'not found in pData(x)'))
+  if (length(unique(pData(x)[,groups])) != 2) stop('pvalTreat only works for 2 groups')
   if (!missing(minCount)) sel <- fData(x)$readCount >= minCount else sel <- rep(TRUE,nrow(x))
-  ans <- double(nrow(x))
-  x <- x[sel,]
-  fit <- fitNNSingleHyp(x, groups=groups, B=5, trace=FALSE) 
-  groups <- pData(x)[,groups]
-  x <- exprs(x)  
-  par <- fit$parest
-  # a.sigma, b.sigma
-  a.sigma <- rep(.5*(par['v0']+ncol(x)), nrow(x))
-  b.sigma <- rep(.5*par['v0']*par['sigma02'], nrow(x))
-  ncolsel <- integer(length(unique(groups)))
-  xbar <- matrix(NA, nrow=nrow(x), ncol=length(ncolsel))
-  names(ncolsel) <- colnames(xbar) <- unique(groups)
-  for (j in unique(groups)) {
-    colsel <- groups %in% j
-    ncolsel[j] <- sum(colsel)
-    xbar[,j] <- rowMeans(x[,colsel])
-    b.sigma <- b.sigma + .5*rowSums((x[,colsel]-xbar[,j])^2)
-  }
-  # function to integrate
-  f <- function(phi) {
-    v <- 1/(ncolsel/phi + 1/par['tau02'])
-    m <- t((ncolsel*t(xbar)/phi + par['mu0']/par['tau02']) * v)
-    pp <- log(pnorm(-logfc, m[,1]-m[,2], sd=sqrt(v[1]+v[2])) + pnorm(logfc, m[,1]-m[,2], sd=sqrt(v[1]+v[2]), lower.tail=FALSE))
-    #exp(pp + dinvgammaVec(phi, a.sigma, b.sigma, logscale=TRUE))
-    exp(pp + dinvgammaVec(phi, a.sigma, b.sigma, logscale=TRUE) - dinvgammaVec(phi, aprop, bprop, logscale=TRUE))
-  }
-  B <- 1000
-  aprop <- a.sigma * min(b.sigma) / mean(b.sigma); bprop <- min(b.sigma)
-  qphi <- rev(1/qgamma((1:B)/(B+1), aprop, bprop))
-  ans[sel] <- rowMeans(do.call(cbind, lapply(qphi, f)))
-  #dprop <- dinvgammaVec(qphi, aprop, bprop, logscale=FALSE)
-  #ans[sel] <- colMeans(do.call(rbind, lapply(qphi, f)) / dprop)
+  ans <- rep(NA,nrow(x)); names(ans) <- featureNames(x)
+  fit <- contrasts.fit(lmFit(x[sel,], design=model.matrix(~ pData(x)[,'group'])), contrasts=c(0,1))
+  ans[sel] <- p.adjust(treat(fit, lfc=logfc)$p.value[,1], method=p.adjust.method)
   return(ans)
-} 
+}
+)
+
