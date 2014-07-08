b2221f129419942dc6adac8bf63de768c368670c
David Rossell <rosselldavid@gmail.com>
2014-07-08 12:47:11 +0000

    mergeExp now works for ExpressionSets with multiple samples each
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/casper@92258 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/mergeExp.R
+++ R/mergeExp.R
@@ -1,25 +1,34 @@
-mergeExp <- function(..., sampleNames,  keep=c('transcript','gene_id','island_id')) {
+mergeExp <- function(..., sampleNames, keep=c('transcript','gene_id','island_id')) {
   esets <- list(...)
   if(class(esets[[1]])=='list') esets <- unlist(esets)
   if (length(unique(sapply(esets,nrow))) != 1) stop('Number of rows do not match')
-  if (any(sapply(esets,ncol) != 1)) stop("All ExpressionSet objects in '...' should have 1 column")
-  if (missing(sampleNames)) sampleNames <- paste('Sample',1:length(esets),sep='')
+  if (any(sapply(esets,ncol) != 1)) {
+    sampleNames <- unlist(lapply(esets, Biobase::sampleNames))
+  } else {   #all ExpressionSets have 1 column
+    if (missing(sampleNames)) {
+      sampleNames <- paste('Sample',1:length(esets),sep='')
+    } else {
+      if (length(sampleNames)!=length(esets)) stop('sampleNames length does not match the input')
+    }
+  }
   if (length(esets)>1) {
     n <- featureNames(esets[[1]])
-    readCount <- rowSums(do.call("cbind",lapply(esets, function(z) fData(z)[n,'explCnts'])))
+    readCount <- rowSums(do.call("cbind",lapply(esets, function(z) rowSums(fData(z)[n,grep('explCnts',fvarLabels(z))]))))
     sel <- !(keep %in% c('transcript','gene_id','island_id'))
-    fData(esets[[1]]) <- fData(esets[[1]])[,keep]
-    fvarLabels(esets[[1]])[sel] <- paste(sampleNames[1],fvarLabels(esets[[1]])[sel],sep='.')
-    sampleNames(esets[[1]]) <- sampleNames[1]
+    cursel <- c('transcript','gene_id','island_id',fvarLabels(esets[[1]])[grep(keep[sel],fvarLabels(esets[[1]]))])
+    fData(esets[[1]]) <- fData(esets[[1]])[,cursel]
+    fvarLabels(esets[[1]])[-1:-3] <- paste(sampleNames[1],fvarLabels(esets[[1]])[-1:-3],sep='.')
+#    sampleNames(esets[[1]]) <- sampleNames[1]
     for (i in 2:length(esets)) {
-      if (any(!(keep %in% fvarLabels(esets[[i]])))) stop("Some variables in argument 'keep' were not found")
-      fData(esets[[i]]) <- fData(esets[[i]])[,keep]
-      sel <- !(keep %in% c('transcript','gene_id','island_id'))
-      fvarLabels(esets[[i]])[sel] <- paste(sampleNames[i],fvarLabels(esets[[i]])[sel],sep='.')
+      #if (any(!(keep %in% fvarLabels(esets[[i]])))) stop("Some variables in argument 'keep' were not found")
+      cursel <- c('transcript','gene_id','island_id',fvarLabels(esets[[i]])[grep(keep[sel],fvarLabels(esets[[i]]))])
+      fData(esets[[i]]) <- fData(esets[[i]])[,cursel]
+      fvarLabels(esets[[i]])[-1:-3] <- paste(sampleNames[i],fvarLabels(esets[[i]])[-1:-3],sep='.')
       esets[[i]] <- esets[[i]][n,]
-      sampleNames(esets[[i]]) <- sampleNames[i]
+#      sampleNames(esets[[i]]) <- sampleNames[i]
     }
     ans <- do.call("combine",esets)
+    sampleNames(ans) <- sampleNames
   } else {
     readCount <- fData(esets[[1]])[,'explCnts']
     ans <- esets[[1]]
