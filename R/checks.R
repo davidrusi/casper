0ddea429746302b0c940ca49695a4ebbea5228aa
Victor Pena <vp58@stat.duke.edu>
2014-09-22 19:12:11 +0000

    some fixes:casperVignettes,checks,and man for simMAEcheck
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/casper@94382 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/checks.R
+++ R/checks.R
@@ -1,26 +1,25 @@
-simMAEcheck <- function(nsim, islandid, burnin=1000, pc, distr, readLength.pilot=readLength, eset.pilot, usePilot=FALSE, retTxsError=FALSE, genomeDB, mc.cores=1, mc.cores.int=1, verbose=FALSE) {
-  if (missing(islandid)) islandid <- names(genomeDB@transcripts)
+simMAEcheck <- function(nsim, islandid, burnin=1000, pc, distr, readLength.pilot, eset.pilot, usePilot=FALSE, retTxsError=FALSE, genomeDB, mc.cores=1, mc.cores.int=1, verbose=FALSE) {
+  if (missing(islandid)) { 
+    islandid <- names(genomeDB@transcripts) 
+  }
   distr.pilot <- distr
   reads = getNreads(pc)
   n = sum(reads[islandid])
   r = readLength.pilot
-  f = mean(rep(as.numeric(names(rep1$distr@lenDis)), rep1$distr@lenDis))
+  f = mean(rep(as.numeric(names(distr@lenDis)), distr@lenDis))
   readLength = r
   U <- NULL
   txe <- list()
   pcs <- list()
   disArray <- vector("list", length(f))
-  for (i in 1:length(disArray)) disArray[[i]] <- casper:::setfragLength(distr, fragLength=f[i])
-  #   disArray <- mapply(function(x, y) casper:::transDistr(distr.pilot, x, y), f, r)
+  for (i in 1:length(disArray)) disArray[[i]] <- setfragLength(distr, fragLength=f[i])
   if (missing(pc)) {
     if (retTxsError) stop("Option retTxsError==TRUE is only available when argument pc is specified")
     if (verbose) cat("Simulating pilot data...\n")
-    #Draw gene expression from eset.pilot
     th <- exprs(eset.pilot)[sample(1:nrow(eset.pilot),length(islandid),replace=TRUE),sample(1:ncol(eset.pilot),1)]
     th <- 2^(th-max(th)); th <- th/sum(th)
     nSimReads <- rmultinom(n=1, size=10^7, prob=th)[,1]
     names(nSimReads) <- islandid
-    #Draw relative isoform expression from symmetric Dir(alpha) prior, alpha=1/number txs
     simpisDirichlet <- function(z) { if (length(z)==1) { z <- 1 } else { z <- rgamma(length(z), shape=1/length(z)); z <- z/sum(z) }; return(z) }
     txs <- unlist(lapply(genomeDB@transcripts[islandid], names))
     pis <- unlist(lapply(genomeDB@transcripts[islandid], simpisDirichlet))
@@ -29,30 +28,28 @@ simMAEcheck <- function(nsim, islandid, burnin=1000, pc, distr, readLength.pilot
   }
   for (j in 1:length(n)) {
     if (length(disArray[[j]])==1) { distrs <- disArray[[j]] } else { distrs <- disArray[[j]][[sample(1:length(disArray[[j]]),1)]] }
-    #nmean <-  mean(rep(as.numeric(names(d@lenDis)), d@lenDis))
-    #nmean <- f[j] - nmean
-    #names(d@lenDis) <- as.numeric(names(d@lenDis)) + round(nmean)
     if(verbose) cat(paste("Generating posterior samples j =",j, "\n"))
-    pis <- casper:::simPost(islandid=islandid, nsim=nsim, distrs=distr.pilot, genomeDB=genomeDB, pc=pc, readLength=readLength.pilot, mc.cores=mc.cores.int*mc.cores, verbose=verbose)
+    pis <- simPost(islandid=islandid, nsim=nsim, distrs=distr.pilot, genomeDB=genomeDB, pc=pc, readLength=readLength.pilot, mc.cores=mc.cores.int*mc.cores, verbose=verbose)
     if(verbose) cat(paste("Running simulations for j =",j, "\n"))
     if(mc.cores.int>1) {
       require(parallel)
       res <- parallel::mclapply(1:nsim, function(i){
         readYield <- runif(1,0.8,1.2) #actual reads +/- 20% within target
-        pmapped <- (1-casper:::probNonMappable(readLength)) * runif(1,0.6,0.9)  #mapped reads 60%-90% of mappable reads
+        pmapped <- (1-probNonMappable(readLength)) * runif(1,0.6,0.9)  #mapped reads 60%-90% of mappable reads
         N <- round(n[j]*readYield*pmapped)
-        sim.pc <-  casper:::simPostPred(islandid=islandid, nreads=N, pis=pis[i,], pc=pc, distrs=distrs, rl=r[j], genomeDB=genomeDB, verbose=verbose)
-        if(usePilot) sim.pc$pc <- casper:::mergePCs(pcs=list(sim.pc$pc,pc), genomeDB=genomeDB)
+        sim.pc <-  simPostPred(islandid=islandid, nreads=N, pis=pis[i,], pc=pc, distrs=distrs, rl=r[j], genomeDB=genomeDB, verbose=verbose)
+        if(usePilot) sim.pc$pc <- mergePCs(pcs=list(sim.pc$pc,pc), genomeDB=genomeDB)
         sim.exp <- exprs(calcExp(islandid=islandid, distrs=distrs, genomeDB=genomeDB, pc=sim.pc$pc, readLength=r[j], rpkm=FALSE, mc.cores=mc.cores))
         list(maes=abs(sim.exp[colnames(pis),]-pis[i,]), pc=sim.pc$pc)
       }, mc.cores=mc.cores.int, mc.preschedule=FALSE)
-    } else {
+    } 
+    else {
       res <- lapply(1:nsim, function(i){
         readYield <- runif(1,0.8,1.2) #actual reads +/- 20% within target
-        pmapped <- (1-casper:::probNonMappable(readLength)) * runif(1,0.6,0.9)  #mapped reads 60%-90% of mappable reads
+        pmapped <- (1-probNonMappable(readLength)) * runif(1,0.6,0.9)  #mapped reads 60%-90% of mappable reads
         N <- round(n[j]*readYield*pmapped)
-        sim.pc <-  casper:::simPostPred(islandid=islandid, nreads=N, pis=pis[i,], pc=pc, distrs=distrs, rl=r[j], genomeDB=genomeDB, verbose=verbose)
-        if(usePilot) sim.pc$pc <- casper:::mergePCs(pcs=list(sim.pc$pc,pc), genomeDB=genomeDB)
+        sim.pc <-  simPostPred(islandid=islandid, nreads=N, pis=pis[i,], pc=pc, distrs=distrs, rl=r[j], genomeDB=genomeDB, verbose=verbose)
+        if(usePilot) sim.pc$pc <- mergePCs(pcs=list(sim.pc$pc,pc), genomeDB=genomeDB)
         sim.exp <- exprs(calcExp(islandid=islandid, distrs=distrs, genomeDB=genomeDB, pc=sim.pc$pc, readLength=r[j], rpkm=FALSE))
         list(maes=abs(sim.exp[colnames(pis),]-pis[i,]), pc=sim.pc$pc)
       })
@@ -60,17 +57,15 @@ simMAEcheck <- function(nsim, islandid, burnin=1000, pc, distr, readLength.pilot
     Un <- do.call(cbind, lapply(res, "[[", "maes"))
     df <- data.frame(MAE= colMeans(Un), Nreads=rep(n[j], nsim), ReadLength=rep(r[j], nsim), frLength=rep(f[j], nsim))
     U <- rbind(U, df)
+    
     if(retTxsError) {
       rownames(Un) <- colnames(pis)
       txe[[j]] <- Un
-      # pcs[[j]] <- lapply(res[sel], "[[", "pc")
       pcs[[j]] <- lapply(res, '[[', 'pc')
     }
     
     if (j==length(n)) { 
       l1 = res
-      # nsim will be typically < 10, 
-      # so this step should be pretty fast
       nrvector = lapply(l1, function(x) getNreads(x$pc)[islandid])
       nrvector = lapply(nrvector, function(x) x[sort(names(x))])
       rown  = names(nrvector[[1]])
@@ -82,16 +77,17 @@ simMAEcheck <- function(nsim, islandid, burnin=1000, pc, distr, readLength.pilot
       s1 = sum(nrpilot >= minim & nrpilot <= maxim)
     }
     
-    
   }
+  
   if(retTxsError){
     names(txe) <- paste(n, r, f, sep='-')
     names(pcs) <- paste(n, r, f, sep='-')
     ans <- list(txe=txe, U=U, pcs=pcs, pis=pis)
-  } else {
-    ## formatting output for model check
+  } 
+  
+  else {
     expected = round(length(islandid)*(nsim-1)/(nsim+1))
     ans <- list(U=U, mod_check=data.frame(Expected=expected, Observed=s1))
   }
-  return(ans)
+  ans
 }
